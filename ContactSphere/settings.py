1d89a14943f13e88976e1704abac125a3411f15b
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-05-24 23:42:42 -0300

    update

--- ContactSphere/settings.py
+++ ContactSphere/settings.py
@@ -65,7 +65,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'https://sphere-contacts.vercel.app'
+    'https://sphere-contacts.vercel.app/',
+    'https://sphere-contacts.vercel.app',
     'https://sphere-contacts-api.vercel.app',
     
 ]
