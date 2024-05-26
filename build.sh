5a309765b33eb478a2c90077c56d05244f622bdd
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-05-26 14:37:00 -0300

    update

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ python3.12 -m pip install -r requirements.txt
 
 echo "Make Migration..."
 python manage.py makemigrations --noinput
-python manage.ph migrate --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
 python manage.py collectstatic --noimput --clear
