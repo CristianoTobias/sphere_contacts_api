9577761b4fd80a3f6e080d4f9b97f2e1c4ce45d2
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-05-26 15:51:23 -0300

    update5

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Ativar ambiente virtual Python
-source /python312/bin/activate
-
 # Instalar as dependências do projeto
-pip install -r requirements.txt
+python pip install -r requirements.txt
 
 # Executar as migrações do banco de dados
 python manage.py makemigrations
