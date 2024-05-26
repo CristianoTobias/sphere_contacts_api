4bcfddc30ad23466fc2f739b75a804df2b8ebf63
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-05-26 14:47:15 -0300

    update2

--- build.sh
+++ build.sh
@@ -3,13 +3,19 @@
 # Abort the script if any command fails
 set -e
 
+# Function to print an error message and exit
+function error_exit {
+    echo "$1" 1>&2
+    exit 1
+}
+
 # Build the project
 echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt || error_exit "Failed to install requirements"
 
 echo "Make Migration..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput || error_exit "Failed to make migrations"
+python3.12 manage.py migrate --noinput || error_exit "Failed to migrate"
 
 echo "Collect Static..."
-python3.12 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear || error_exit "Failed to collect static files"
