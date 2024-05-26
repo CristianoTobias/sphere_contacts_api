dedf9609509353205d3e7963be8b630b0cf30789
Cristiano Tobias <cristiano.tobias40@gmail.com>
2024-05-26 19:44:58 -0300

    create superuser

--- build.sh
+++ build.sh
@@ -13,3 +13,20 @@ python3.12 manage.py migrate --noinput
 
 echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
+
+# Criação do superusuário usando variáveis de ambiente
+echo "Creating superuser..."
+python3.12 manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.getenv('SUPERUSER_NAME')
+email = os.getenv('SUPERUSER_EMAIL')
+password = os.getenv('SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
+
+echo "Superuser created successfully."
